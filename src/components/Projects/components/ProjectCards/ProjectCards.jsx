5efbef7174bdc1d5fed56a3ee515d264f7c061f0
alexandracampo <alexandracampo86@gmail.com>
2025-03-19 12:15:19 +0100

    change styles

--- src/components/Projects/components/ProjectCards/ProjectCards.jsx
+++ src/components/Projects/components/ProjectCards/ProjectCards.jsx
@@ -2,13 +2,13 @@ import React, { useRef } from "react";
 import "./project-cards.scss";
 
 const projects = [
-    { id: 1, title: "Prime Cleaning", link: "https://github.com/alexandracampo/prime-cleaning", img: "cleaning.JPG" },
-    { id: 2, title: "Memory", link: "https://github.com/alexandracampo/memory", img: "memory.jpg" },
-    { id: 3, title: "Adivina el número", link: "https://github.com/alexandracampo/adivina-el-numero", img: "number.jpg" },
-    { id: 4, title: "Consulta el tiempo", link: "https://github.com/alexandracampo/weatherInfo", img: "weather.jpg" },
-    { id: 5, title: "To do list", link: "https://github.com/alexandracampo/lista-tareas", img: "todo.jpg" },
-    { id: 6, title: "Tus proyectos", link: "https://github.com/alexandracampo/web-de-proyectos", img: "proyectos.JPG" },
-    { id: 6, title: "Tu tarjeta de visita", link: "https://github.com/alexandracampo/web-tarjetas-de-visita", img: "tarjetas.JPG" },
+    { id: 1, title: "Prime Cleaning", link: "https://alexandracampo.github.io/prime-cleaning/", img: "cleaning.JPG" },
+    { id: 2, title: "Memory", link: "https://alexandracampo.github.io/memory/", img: "memory.jpg" },
+    { id: 3, title: "Adivina el número", link: "https://alexandracampo.github.io/adivina-el-numero/", img: "number.jpg" },
+    { id: 4, title: "Consulta el tiempo", link: "https://alexandracampo.github.io/weatherInfo/", img: "weather.jpg" },
+    { id: 5, title: "To do list", link: "https://alexandracampo.github.io/lista-tareas/", img: "todo.jpg" },
+    { id: 6, title: "Tus proyectos", link: "https://alexandracampo.github.io/web-de-proyectos/", img: "proyectos.JPG" },
+    { id: 6, title: "Tu tarjeta de visita", link: "https://alexandracampo.github.io/web-tarjetas-de-visita/", img: "tarjetas.JPG" },
 ];
 
 const ProjectCards = () => {
@@ -22,32 +22,37 @@ const ProjectCards = () => {
     };
 
     return (
-        <div className="carousel-container" id="projects"  >
-            <h3 className="title-projects"> Mis <span className="color-text">proyectos</span> en GitHub: </h3>
-
-            <button className="scroll-button left" onClick={() => scrollCarousel(-1)}>
-                {/*  &lt; */}
-            </button>
-
-            <div className="carousel" ref={carouselRef}>
-                {projects.map((project) => (
-                    <a
-                        key={project.id}
-                        href={project.link}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="card"
-                    >
-                        <img src={project.img} alt={project.title} />
-                        <h3 className="title-card">{project.title}</h3>
-                    </a>
-                ))}
+        <>
+            <div id="projects" className="header-projects">
+                <h3 className="title-projects"> Mis <span className="color-text">proyectos</span> en GitHub: </h3>
+                <p className="text-projects">Aquí tienes acceso a mis proyectos, algunos están en proceso de construcción, así que no te asustes si ves un diseño que parece de los 90</p>
             </div>
 
-            <button className="scroll-button right" onClick={() => scrollCarousel(1)}>
-                {/* &gt; */}
-            </button>
-        </div>
+            <div className="carousel-container"   >
+                <button className="scroll-button left" onClick={() => scrollCarousel(-1)}>
+                    {/*  &lt; */}
+                </button>
+
+                <div className="carousel" ref={carouselRef}>
+                    {projects.map((project) => (
+                        <a
+                            key={project.id}
+                            href={project.link}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="card"
+                        >
+                            <img src={project.img} alt={project.title} />
+                            <h3 className="title-card">{project.title}</h3>
+                        </a>
+                    ))}
+                </div>
+
+                <button className="scroll-button right" onClick={() => scrollCarousel(1)}>
+                    {/* &gt; */}
+                </button>
+            </div>
+        </>
     );
 };
 
